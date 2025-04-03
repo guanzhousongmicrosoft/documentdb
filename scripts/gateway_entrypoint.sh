e817b83a61fac99b406c0be69f5a21e5fbac2489
guanzhousong <guanzhousong@microsoft.com>
2025-04-03 22:24:49 +0000

    fix

--- scripts/gateway_entrypoint.sh
+++ scripts/gateway_entrypoint.sh
@@ -1,4 +1,4 @@
 ./scripts/start_oss_server.sh -c > ~/oss_server.log 2>&1 & 
 wait # Wait for the first script to complete
 
-./scripts/build_and_start_rust_gateway_oss.sh -u  > ~/rust_gateway.log 2>&1 &
+./scripts/build_and_start_rust_gateway_oss.sh -u -d ~/code/pg_documentdb_gw/SetupConfiguration.json > ~/rust_gateway.log 2>&1 &
