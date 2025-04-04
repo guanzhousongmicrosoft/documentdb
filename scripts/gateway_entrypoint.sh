9440544e6b53d7f0a4efab698e7f29e89191914d
guanzhousong <guanzhousong@microsoft.com>
2025-04-04 18:41:57 +0000

    update

--- scripts/gateway_entrypoint.sh
+++ scripts/gateway_entrypoint.sh
@@ -1,4 +1,9 @@
-./scripts/start_oss_server.sh -c > ~/oss_server.log 2>&1 & 
-wait # Wait for the first script to complete
+#!/bin/bash
 
-./scripts/build_and_start_rust_gateway_oss.sh -u -d ~/code/pg_documentdb_gw/SetupConfiguration.json > ~/rust_gateway.log 2>&1 &
+echo "Starting OSS server..."
+./scripts/start_oss_server.sh -c > /home/documentdb/oss_server.log 2>&1
+wait # Wait for the OSS server script to finish
+echo "OSS server started."
+
+echo "Starting Rust gateway..."
+./scripts/build_and_start_rust_gateway_oss.sh -u -d ~/code/pg_documentdb_gw/SetupConfiguration.json > /home/documentdb/rust_gateway.log 2>&1 &
